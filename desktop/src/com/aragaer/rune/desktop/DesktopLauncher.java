87e7ff6fe77f3b032554e040a77527cd2f961db6
aragaer <aragaer@gmail.com>
2016-01-01 08:02:05 +0300

    Base libgdx project

---
+++ desktop/src/com/aragaer/rune/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.aragaer.rune.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.aragaer.rune.RuneGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RuneGame(), config);
+	}
+}
